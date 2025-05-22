1b1ab74d1e90f8d88a7aaea32a31fdadd180ccc3
Carla_Diana <carladianavgonzales@gmail.com>
2025-05-22 09:43:00 -0400

    Refactoring: Se elimina ifs anidados

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,19 +1,13 @@
 function calcularElFizzBuzzDeUnNumero(n) {
-  let fb = "";
-  if (esMultiploDe(n, 3) && esMultiploDe(n, 5)) {
-    fb = "FizzBuzz";
-  } else {
-    if (esMultiploDe(n, 3)) {
-      fb = "Fizz";
-    } else {
-      if (esMultiploDe(n, 5)) {
-        fb = "Buzz";
-      } else {
-        fb = n.toString();
-      }
-    }
-  }
-  return fb;
+
+  if (esMultiploDe(n, 3) && esMultiploDe(n, 5)) return "FizzBuzz";
+  
+  if (esMultiploDe(n, 3))  return "Fizz";
+
+  if (esMultiploDe(n, 5)) return "Buzz";
+  
+  return n.toString();
+  
 }
 function esMultiploDe(n, multiplo) {
   return n % multiplo === 0;
